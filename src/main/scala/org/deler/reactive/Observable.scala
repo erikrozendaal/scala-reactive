1ca0cd47c47e0f0a38471171577161dca1dc1b1d
Erik Rozendaal <erik@deler.org>
2010-10-23 14:52:08 +0200

    Merged ObservableLike back into Observable.

--- src/main/scala/org/deler/reactive/Observable.scala
+++ src/main/scala/org/deler/reactive/Observable.scala
@@ -2,61 +2,101 @@ package org.deler.reactive
 
 import scala.collection._
 
-trait Observable[+E] extends ObservableLike[E] {
+/**
+ * An observable can be subscribed to by an [[org.deler.reactive.Observer]]. Observables produce zero or more values
+ * using <code>onNext</code> optionally terminated by either a call to <code>onCompleted</code> or <code>onError</code>.
+ */
+trait Observable[+A] {
   self =>
   import Observable._
+
+  /**
+   * Subscribes <code>observer</code>.
+   */
+  def subscribe(observer: Observer[A]): Subscription
+
+  /**
+   * Subscribes <code>onNext</code> to every value produced by this Observable.
+   */
+  def subscribe(onNext: A => Unit): Subscription = subscribe(onNext, defaultOnError, defaultOnCompleted)
+
+  /**
+   * Subscribes <code>onNext</code> and <code>onError</code> to this Observable.
+   */
+  def subscribe(onNext: A => Unit, onError: Exception => Unit): Subscription = subscribe(onNext, onError, defaultOnCompleted)
+
+  /**
+   * Subscribes <code>onNext</code> and <code>onCompleted</code> to this Observable.
+   */
+  def subscribe(onNext: A => Unit, onCompleted: () => Unit): Subscription = subscribe(onNext, defaultOnError, onCompleted)
+
+  /**
+   * Subscribes <code>onNext</code>, <code>onError</code>, and <code>onCompleted</code> to this Observable.
+   */
+  def subscribe(onNext: A => Unit, onError: Exception => Unit, onCompleted: () => Unit): Subscription = {
+    val completedCallback = onCompleted
+    val errorCallback = onError
+    val nextCallback = onNext
+    this subscribe (new Observer[A] {
+      override def onCompleted() = completedCallback()
+
+      override def onError(error: Exception) = errorCallback(error)
+
+      override def onNext(value: A) = nextCallback(value)
+    })
+  }
 
   /**
    * A new observable defined by applying a partial function to all values produced by this observable on which the
    * function is defined.
    */
-  def collect[F](pf: PartialFunction[E, F]): Observable[F] = {
+  def collect[B](pf: PartialFunction[A, B]): Observable[B] = {
     for (event <- this if pf.isDefinedAt(event)) yield pf(event)
   }
 
   /**
    * A new observable only containing the values from this observable for which the predicate is satisfied.
    */
-  def filter(p: E => Boolean): Observable[E] = createWithSubscription {
+  def filter(p: A => Boolean): Observable[A] = createWithSubscription {
     observer =>
-      self.subscribe(new Observer[E] {
+      self.subscribe(new Observer[A] {
         override def onCompleted() = observer.onCompleted()
 
         override def onError(error: Exception) = observer.onError(error)
 
-        override def onNext(event: E) = if (p(event)) observer.onNext(event)
+        override def onNext(event: A) = if (p(event)) observer.onNext(event)
       })
   }
 
   /**
    * Alias for <code>filter</code> that is used by the Scala for-loop construct.
    */
-  def withFilter(p: E => Boolean) = filter(p)
+  def withFilter(p: A => Boolean) = filter(p)
 
   /**
    * A new observable defined by applying a function to all values produced by this observable.
    */
-  def map[F](f: E => F): Observable[F] = createWithSubscription {
+  def map[B](f: A => B): Observable[B] = createWithSubscription {
     observer =>
-      self.subscribe(new Observer[E] {
+      self.subscribe(new Observer[A] {
         override def onCompleted() = observer.onCompleted()
 
         override def onError(error: Exception) = observer.onError(error)
 
-        override def onNext(event: E) = observer.onNext(f(event))
+        override def onNext(event: A) = observer.onNext(f(event))
       })
   }
 
   /**
    * A new observable that only contains the values from this observable which are instances of the specified type.
    */
-  def ofType[F](clazz: Class[F]): Observable[F] = {
+  def ofType[B](clazz: Class[B]): Observable[B] = {
     for (value <- this if clazz.isInstance(value)) yield clazz.cast(value)
   }
 
-  def observeOn(scheduler: Scheduler): Observable[E] = createWithSubscription {
+  def observeOn(scheduler: Scheduler): Observable[A] = createWithSubscription {
     observer =>
-      self.subscribe(new Observer[E] {
+      self.subscribe(new Observer[A] {
         override def onCompleted() = scheduler schedule {
           observer.onCompleted()
         }
@@ -65,7 +105,7 @@ trait Observable[+E] extends ObservableLike[E] {
           observer.onError(error)
         }
 
-        override def onNext(event: E) = scheduler schedule {
+        override def onNext(event: A) = scheduler schedule {
           observer.onNext(event)
         }
       })
@@ -74,28 +114,28 @@ trait Observable[+E] extends ObservableLike[E] {
   /**
    * A new observable that executes <code>action</code> for its side-effects for each value produced by this observable.
    */
-  def perform(action: E => Unit): Observable[E] = createWithSubscription {
+  def perform(action: A => Unit): Observable[A] = createWithSubscription {
     observer =>
-      self.subscribe(new Observer[E] {
+      self.subscribe(new Observer[A] {
         override def onCompleted() = observer.onCompleted()
 
         override def onError(error: Exception) = {
           observer.onError(error)
         }
 
-        override def onNext(event: E) = {
+        override def onNext(event: A) = {
           action(event);
           observer.onNext(event)
         }
       })
   }
 
-  def take(n: Int): Observable[E] = createWithSubscription {
+  def take(n: Int): Observable[A] = createWithSubscription {
     observer =>
       self.subscribe(new RelayObserver(observer) {
         var count: Int = 0
 
-        override def onNext(event: E) = {
+        override def onNext(event: A) = {
           if (count < n) {
             super.onNext(event)
             count += 1
@@ -107,22 +147,22 @@ trait Observable[+E] extends ObservableLike[E] {
       })
   }
 
-  def materialize: Observable[Notification[E]] = createWithSubscription {
+  def materialize: Observable[Notification[A]] = createWithSubscription {
     observer =>
-      self.subscribe(new Observer[E] {
+      self.subscribe(new Observer[A] {
         override def onCompleted() = observer.onNext(OnCompleted)
 
         override def onError(error: Exception) = observer.onNext(OnError(error))
 
-        override def onNext(value: E) = observer.onNext(OnNext(value))
+        override def onNext(value: A) = observer.onNext(OnNext(value))
       })
   }
 
   // only works for immediate observables!
-  def toSeq: Seq[E] = {
-    val result = new mutable.ArrayBuffer[E]
-    self.subscribe(new Observer[E] {
-      override def onNext(event: E) {result append event}
+  def toSeq: Seq[A] = {
+    val result = new mutable.ArrayBuffer[A]
+    self.subscribe(new Observer[A] {
+      override def onNext(event: A) {result append event}
     }).close()
     result
   }
@@ -130,6 +170,23 @@ trait Observable[+E] extends ObservableLike[E] {
 }
 
 object Observable {
+  /**
+   * The default <code>onNext</code> handler does nothing.
+   */
+  def defaultOnNext[T](value: T) {}
+
+  /**
+   * The default <code>onError</code> handler throws the <code>error</code>.
+   */
+  def defaultOnError(error: Exception) {
+    throw error
+  }
+
+  /**
+   * The default <code>onCompleted</code> handler does nothing.
+   */
+  def defaultOnCompleted() {}
+
   def noop() {}
 
   def create[T](delegate: Observer[T] => () => Unit): Observable[T] = new Observable[T] {
