0b5538555601a99db3a1fc4154a3a2f0aae0d12d
Erik Rozendaal <erik@deler.org>
2010-11-11 17:57:13 +0100

    Differentiate between two times in Merge example.

--- src/main/scala/org/deler/reactive/example/Merge.scala
+++ src/main/scala/org/deler/reactive/example/Merge.scala
@@ -4,12 +4,12 @@ import org.deler.reactive.Observable
 import org.joda.time.{LocalDateTime, Duration}
 
 object Merge extends Application {
-  val every200ms = Observable.interval(new Duration(200))
-  val every300ms = Observable.interval(new Duration(300))
+  val every200ms = Observable.interval(new Duration(200)).map(n => "timer 1, tick " + n)
+  val every300ms = Observable.interval(new Duration(300)).map(n => "timer 2, tick " + n)
 
   val merged = Observable(every200ms, every300ms).flatten.take(10)
 
   merged subscribe { n =>
-    println("Received tick " + n + " at " + new LocalDateTime().toString("HH:mm:ss.SSS"))
+    println("Received " + n + " at " + new LocalDateTime().toString("HH:mm:ss.SSS"))
   }
 }
