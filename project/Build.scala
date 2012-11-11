a0bf955d7f753454b2cd31a01fd31da96b322501
Erik Rozendaal <erik@deler.org>
2012-11-11 22:06:04 +0100

    Upgraded dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -13,14 +13,14 @@ object ProjectBuild extends Build {
       scalaVersion := "2.9.2",
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
       libraryDependencies ++= Seq(
-        "joda-time" % "joda-time" % "1.6.2",
-        "org.slf4j" % "slf4j-api" % "1.6.1",
-        "org.slf4j" % "slf4j-log4j12" % "1.6.1",
+        "joda-time" % "joda-time" % "2.1",
+        "org.joda" % "joda-convert" % "1.2",
+        "org.slf4j" % "slf4j-api" % "1.6.6",
+        "org.slf4j" % "slf4j-log4j12" % "1.6.6",
         "net.liftweb" % "lift-json_2.9.1" % "2.4",
-        "com.ning" % "async-http-client" % "1.4.0",
+        "com.ning" % "async-http-client" % "1.7.6",
         "junit" % "junit" % "4.10" % "test",
         "org.scala-tools.testing" % "specs_2.9.1" % "1.6.9" % "test",
         "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
-        "org.mockito" % "mockito-all" % "1.8.5" % "test",
-        "junit" % "junit" % "4.10" % "test")))
+        "org.mockito" % "mockito-all" % "1.9.0" % "test")))
 }
